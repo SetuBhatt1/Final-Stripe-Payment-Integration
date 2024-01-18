2cb907b192a99901198e62d2d56de103a427f1d8
Setu Bhatt <bansetu1@gmail.com>
2024-01-18 13:13:49 +0530

    changed frontend url

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.post("/checkout", async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: lineItems,
             mode: 'payment',
-            success_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel",
+            success_url: "https://final-stripe-payment-integration.vercel.app/success",
+            cancel_url: "https://final-stripe-payment-integration.vercel.app/cancel",
         });
 
         res.send(JSON.stringify({
