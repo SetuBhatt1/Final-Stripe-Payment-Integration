4061833f3bda6f429b43c6655ba9753973efefcf
Setu Bhatt <bansetu1@gmail.com>
2024-01-19 08:55:10 +0530

    remove .env from config in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
